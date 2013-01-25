f2b55a76cd39567e3f0f66e6b364f2c775bb38cf
Link <linkkingjay@gmail.com>
2013-01-25 09:53:42 +0800

    Update gdut-jwgl-helper.js
    添加功能：自动填写验证码（有bug）。

--- gdut-jwgl-helper.js
+++ gdut-jwgl-helper.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name       GDUT教务管理系统helper
+// @name       GDUT 教务管理系统 helper
 // @namespace  http://use.i.E.your.homepage/
 // @version    0.1
 // @description  hack
@@ -19,6 +19,7 @@ var helper = {
     total:0
 }
 
+//显示平均绩点和平均分
 function ShowAvgPoint(){
     var table = $('#DataGrid1');
     if(table == null) return;
@@ -52,10 +53,10 @@ function ShowAvgPoint(){
     var tb = $('tbody')[0];
     var lastrow = document.createElement('tr');
     var td1 = document.createElement('td');
-    td1.innerHTML = "Æ½¾ù¼¨µã£º" + helper.avgPoint;
+    td1.innerHTML = "平均绩点：" + helper.avgPoint;
     td1.colSpan = "2";
     var td2 = document.createElement('td');
-    td2.innerHTML = "Æ½¾ù·Ö£º" + helper.avgScore;
+    td2.innerHTML = "平均分：" + helper.avgScore;
     td2.colSpan = "2";
     lastrow.appendChild(td1);
     lastrow.appendChild(td2);
@@ -64,10 +65,94 @@ function ShowAvgPoint(){
 }
 
 
+//填写验证码
+function fillCaptcha()
+{
+    //var img = $("[src='CheckCode.aspx']");
+    var imgs = document.getElementsByTagName("img");
+    var img = imgs[3];
+    if(img == null) {return;}
+    img.onload = function(){
+        var code = getCode(img);
+        $("#TextBox3").val(code);
+    }
+}
+
+function getCode(img){
+    var code = "";
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img,0,0);
+    var c = ctx.getImageData(0,0,img.width,img.height);
+    for(i=0; i<c.height; i++){
+        for(j=0; j<c.width; j++){
+            var x = (i*4)*c.width+(j*4); 
+            var r = c.data[x];
+            var g = c.data[x+1];
+            var b = c.data[x+2];
+            if(r+g+b > 350){
+                c.data[x] = c.data[x+1] = c.data[x+2] = 0;
+            }
+            else{
+                c.data[x] = c.data[x+1] = c.data[x+2] = 255;
+            }
+        }
+    }
+    for(var i=0;i<5;i++)
+    {
+        var x1 = i*9+5;
+        var x2 = i*9+13;
+        code += getNum(c,x1,5,x2,17);
+    }
+    return code;
+}
+
+
+function getNum(imgData,x1,y1,x2,y2){
+    var num = 0;
+    for(i=y1; i<y2; i++){
+        for(j=x1; j<x2; j++){
+            var x = (i*4)*imgData.width+(j*4);
+            if(imgData.data[x] == 255)num++;
+        }
+    }
+    switch(num)
+    {
+        case 56:{
+            j = (x1+x2)/2;
+            i = (y1+y2)/2;
+            var x = (i*4)*imgData.width+(j*4);
+            if(imgData.data[x] == 255)
+                return 8;
+            else
+                return 0;
+        }
+        case 30:return 1;
+        case 50:return 2;
+        case 51:return 3;
+        case 48:return 4;
+        case 57:return 5;
+        case 58:{
+            i = y2-2;
+            j = x1;
+            var x = (i*4)*imgData.width+(j*4);
+            if(imgData.data[x] == 255)
+                return 9;
+            else
+                return 6;
+        }
+        case 37:return 7;
+        default:return 0;
+    }
+}
 
 function init(){
     document.onmousedown = null;
     ShowAvgPoint();
+    fillCaptcha();
 }
 
 init();
