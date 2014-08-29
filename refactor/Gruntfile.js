4c17c560124cacc56eef6d973d4ebdd2b84f6769
hbc <bcxxxxxx@gmail.com>
2014-08-29 21:52:57 +0800

    完成基本开发部件搭建。

---
+++ refactor/Gruntfile.js
@@ -0,0 +1,128 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        
+        // 脚本基本信息
+        pkg: grunt.file.readJSON('package.json'),
+        
+        // 文件夹路径
+        dir: {
+            src: './src',
+            manifest: './src/manifest',
+            build: './build',
+            dist: './'
+        },
+
+        // 任务配置
+
+        clean: {
+            build: ['<%= dir.build %>']
+        },
+
+        concat: {
+            build_gm: {
+                src: [
+                    '<%= dir.manifest %>/greasemonkey.js',
+                    '<%= dir.src %>/gdut-jwgl-helper.js'
+                ],
+                dest: '<%= dir.build %>/gdut-jwgl-helper.gm.js'
+            }
+        },
+
+        copy: {
+            build_crx: {
+                files: [
+                    {
+                        src: '<%= dir.src %>/gdut-jwgl-helper.js',
+                        dest: '<%= dir.build %>/crx/gdut-jwgl-helper.js'
+                    },
+                    {
+                        src: '<%= dir.manifest %>/crx.json',
+                        dest: '<%= dir.build %>/crx/manifest.json'
+                    },
+                    {
+                        src: '<%= dir.src %>/vendor/jquery.min.js',
+                        dest: '<%= dir.build %>/crx/jquery.min.js'
+                    }
+                ]
+            },
+
+            publish_gm: {
+                src: '<%= dir.build %>/gdut-jwgl-helper.gm.js',
+                dest: '<%= dir.dist %>/gdut-jwgl-helper.<%= pkg.version %>.js'
+            },
+
+            publish_crx: {
+                src: '<%= dir.build %>/crx.crx',
+                dest: '<%= dir.dist %>/gdut-jwgl-helper.<%= pkg.version %>.crx'
+            }
+        },
+
+        shell: {
+            build_crx: {
+                command: function () {
+                    var pem = grunt.template.process(
+                            '<%= dir.manifest %>/gdut-jwgl-helper.pem'
+                    ),
+                        cmd = 'chromium --pack-extension=<%= dir.build %>/crx';
+
+                    if (grunt.file.exists(pem)) {
+                        cmd += ' --pack-extension-key=' + pem;
+                    } else {
+                        cmd += '&& mv <%= dir.build %>/crx.pem ' + pem;
+                    }
+
+                    return cmd;
+                }
+            }
+        },
+
+        watch: {
+            src: {
+                files: ['<%= dir.src %>/**/*.js', '<%= dir.src %>/**/*.json'],
+                tasks: ['package']
+            }
+        }
+
+    });
+
+    // 加载 grunt 的插件
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
+
+    // 定义任务
+
+    // 将脚本打包成 crx 格式
+    grunt.registerTask('build_crx', [
+        'copy:build_crx',
+        'shell:build_crx'
+    ]);
+
+    // 将脚本打包成 greasemonkey 脚本格式
+    grunt.registerTask('build_gm', [
+        'concat:build_gm'
+    ]);
+
+    // 打包任务
+    grunt.registerTask('package', [
+        'clean:build',
+        'build_gm',
+        'build_crx'
+    ]);
+
+    // 发布任务
+    grunt.registerTask('publish', [
+        'package',
+        'copy:publish_gm',
+        'copy:publish_crx'
+    ]);
+
+    // 默认任务：检测代码改动并自动打包
+    grunt.registerTask('default', [
+        'watch'
+    ]);
+
+};
---
+++ refactor/src/manifest/greasemonkey.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        GDUT 教务管理系统 helper
+// @namespace   https://github.com/vtmer/gdut-jwgl-helper
+// @version     0.2.0
+// @description	make jwgl.gdut.edu.cn better.
+// @match       http://jwgl.gdut.edu.cn/*
+// @match       http://jwgldx.gdut.edu.cn/*	
+// @match       http://222.200.98.201/*
+// @match       http://222.200.98.204/*
+// @match       http://222.200.98.205/*
+// @match       http://222.200.98.206/*
+// @copyright   2013, VTM STUDIO
+// @require     http://cdn.staticfile.org/jquery/2.1.1-rc2/jquery.min.js
+// ==/UserScript==
